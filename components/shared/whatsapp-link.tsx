2c87bd89e96e9e7aa1a41de42849de6d7abce828
Benedict <benedictumeozor@gmail.com>
2024-11-27 09:33:34 +0100

    update

--- components/shared/whatsapp-link.tsx
+++ components/shared/whatsapp-link.tsx
@@ -7,7 +7,7 @@ const WhatsAppLink = () => {
   return (
     <div
       role='button'
-      className='fixed right-8 top-[80%] z-[9999999] grid h-14 w-14 cursor-pointer place-items-center rounded-full bg-green-400 shadow hover:bg-green-500'
+      className='fixed right-8 top-[80%] z-[9999999] grid h-14 w-14 animate-bounce cursor-pointer place-items-center rounded-full bg-green-400 shadow duration-1000 hover:bg-green-500'
     >
       <Image
         src={icon}
